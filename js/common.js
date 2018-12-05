372f66bd620cb94f16e8f595266fa34da6f074da
ljhmw6 <chorika@naver.com>
2018-12-05 18:01:41 +0900

    1차 검수 수정 및 진료안내 작업

--- js/common.js
+++ js/common.js
@@ -1,114 +1,198 @@
-
-
 $(document).ready(function() {
-	var $allBtn=$(".btn-allmenu");
-	var $closeBtn=$(".btn-close");
-	var $header = $('.header');  
-	var $footer = $('.footer'); 
-	var $nav = $('.nav-btn'); 
-	var $mainWrap=$(".main-wrap");
-	var $opensectionWrap=$(".opensection-wrap");
-	var $container=$(".container");
-	var $gnb2depth=$(".gnb-2depth a");
-	var $gnb3Wrap=$(".gnb3-wrap");
-
+	var $allBtn=$(".btn-allmenu"),
+	$closeBtn=$(".btn-close"),
+	$header = $('.header'),  
+	$footer = $('.footer'), 
+	$nav = $('.nav-btn'), 
+	$navWrap=$(".nav-wrap"),
+	$mainWrap=$(".main-wrap"),
+	$opensectionWrap=$(".opensection-wrap"),
+	$container=$(".container"),
+	$gnb2depth=$(".gnb-2depth a"),
+	$gnb3Wrap=$(".gnb3-wrap"),
+	$gnb=$(".gnb"),
+	$tablist1=$(".tablist-type1"),
+	$tablist2=$(".tablist-type2"),
+	$qna=$(".qna"),
+	$location=$(".location-nav-wrap");
 
 	var allBtnNum = 0;	//전체메뉴 기본 설정값
-	var mainY = 'N';	//메인여부 기본 설정값
 
 
 
-	//메인 메뉴 
-	function openMenu(){	
-		$opensectionWrap.slideDown("fast");
+	//메인 확인
+	var mainCheck = function(){
+		if ($('#main').length==1) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//메뉴 열기
+	var openMenu = function(){	
+		$opensectionWrap.slideDown("fast")
+		.addClass('active');
 		$allBtn.hide();
 		$closeBtn.show();
-		 if($header.attr('class')=='header main-style'){
-		 	$header.removeClass('main-style');
-		 	mainY = 'Y';
-		 }
-
+		if (mainCheck()) {
+			$header.removeClass('main-style');
+		}
 	}
-
-	function closeMenu(){		
+	//메뉴 닫기
+	var closeMenu = function(){		
 		if (allBtnNum==0) {
-			$opensectionWrap.stop().slideUp("fast");
+			$opensectionWrap.stop().slideUp("fast")
+			.removeClass('active');
 			$allBtn.show();
 			$closeBtn.hide();
-			 if (mainY=='Y' && $(this).scrollTop() == 0) {				
-			 	$header.addClass('main-style');			
-			 }
+			if (mainCheck() && $(this).scrollTop() == 0) {				
+				$header.addClass('main-style')
+				.removeClass('main-style2')			
+			}
 		}
 	}
 
-
 	//전체버튼 클릭 이벤트
-	$allBtn.click(function(event) {
+	$allBtn.on('click', function(event) {
 		allBtnNum = 1;	
 		openMenu();
 	});
-	$closeBtn.click(function(event) {
+	$closeBtn.on('click', function(event) {
 		allBtnNum = 0;	
 		closeMenu();
 	});
-	$mainWrap.click(function(event) {
+	$mainWrap.on('click', function(event) {
 		allBtnNum = 0;	
 		closeMenu();
 	});
-	$container.click(function(event) {
+	$container.on('click', function(event) {
 		allBtnNum = 0;	
 		closeMenu();
 	});
-
 
 	//메뉴 롤오버 이벤트 
-	$(".gnb").mouseenter(function(){
+	$gnb.on('mouseenter',function(){
+		openMenu();	
+	});
+	$navWrap.on('mouseleave',function(){
+		closeMenu();	
+	});
+	$gnb.find('a').on('focus',function(){
 		openMenu();
 	});
-	$(".nav-wrap").mouseleave(function(){
+	$gnb2depth.last().blur(function(){
 		closeMenu();	
 	});
 
-
 	//메뉴 - 2depth
-	$gnb2depth.mouseenter(function(){
-		$(this).addClass('active');
-		$(this).siblings('gnb3Wrap').show();
+	$gnb2depth.on('mouseenter',function(){
+		$(this).addClass('active')
+		.siblings('gnb3Wrap').show();
 	});
-	$gnb2depth.mouseleave(function(){		
+	$gnb2depth.on('mouseleave',function(){		
 		$(this).removeClass('active');
 	});
 
 	//메뉴 - 3depth
-	$gnb3Wrap.mouseenter(function(){
-		$(this).siblings('a').addClass('active');
-		$(this).show();
+	$gnb3Wrap.on('mouseenter',function(){
+		$(this).siblings('a').addClass('active')
+		.show();
 	});
-	$gnb3Wrap.mouseleave(function(){		
-		$(this).siblings('a').removeClass('active');
-		$(this).hide();
+	$gnb3Wrap.on('mouseleave',function(){		
+		$(this).siblings('a').removeClass('active')
+		.hide();
 	});
+
+
+	//로케이션 이벤트
+	$location.find('a').on('focus mouseenter', function(event) {
+		$(this).addClass('active');
+	});
+	$location.on('blur mouseleave', function(event) {
+		$(this).find('a').removeClass('active');
+	});
+
+
 
 
 
 
 
 	// 탭 갯수가 6개 이하일때 넓이 조절 이하일때 조절바 노출
-	var tapN = $(".tablist-type1 a").length;
+	var tapN = $tablist1.find('a').length;
 	if (tapN < 6) {
 		var tapW = 1180/tapN;
-		$(".tablist-type1 a").css( 'width', tapW+'px' );
+		$tablist1.find('a').css( 'width', tapW+'px' );
 	}else if(tapN > 6){
 		$(".scroll-btn").addClass('active')
 	}
 
 
+	//탭 클릭 이벤트 
+	$tablist1.find('a').on('click',function(e) {
+		e.preventDefault();		
+		$tablist1.find('a').removeClass('active');
+		$(this).addClass('active');
+		$(".tabpanel").removeClass('active');
+		var panel = $(this).attr('href');
+		//alert(panel);
+		$(panel).addClass('active');
+	});
+
+
+	//서브 탭 클릭 이벤트 
+	$tablist2.find('a').on('click',function(e) {
+		e.preventDefault();		
+		$tablist2.find('a').removeClass('active');
+		$(this).addClass('active');
+		$(".tabpanel-sub").removeClass('active');
+		var panel = $(this).attr('href');
+		$(panel).addClass('active');
+	});
+
+
+	//탭 가로 스크롤 버튼
+	$('.scroll-left').on('click keydown', function(event) {
+		scrollMove(-196);
+
+	});
+	//탭 가로 스크롤 버튼
+	$('.scroll-right').on('click keydown', function(event) {
+		scrollMove(196);
+	});
+
+	var scrollMove = function (n){
+		$('.tablist-type1').animate({scrollLeft : $('.tablist-type1').scrollLeft() + n}, 500);   
+	}
+
+
+
+
+  	//자주하는 질문
+  	$qna.find('dt').on('click focus keydown',function() {
+  		$qna.find('dt').removeClass('active');
+  		$qna.find('dd').hide();
+  		$(this).addClass('active');
+  		$(this).next('dd').slideDown("fast");
+  	});
+
 
 	//진료과 목록  아이콘 노출 
-	$('.text-ko > a').focus(function(){
+	$('.text-ko > a').on('focus',function(){
 		$('.over-icon').css( 'display', 'none' );
 		$(this).siblings('.over-icon').css( 'display', 'block' );
 	});
+
+
+	//select IE9 수정
+	if ($("select").length){ 		
+		$('select').each(function(i, e){
+			var sele = $(this).prop('disabled');		
+			if (sele==true) {		 			
+				$(this).parent('label').addClass('disabled');
+			}
+		});
+	}
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-  var $header = $('.header');  
-  var $footer = $('.footer'); 
-  var $nav = $('.nav-btn'); 
+
+
 
 $(document).ready(function() {
+  var $header = $('.header'),  
+  $footer = $('.footer'),
+  $navBtn = $(".nav-btn a"),
+  $service = $('.main-service > div'),
+  $html = $('html, body');
+
 
 
     //메인 클래스 적용(임시) 개발에서 적용 요망
     //현제 화면 오픈시 class 적용 전 화면이 보이는 문제  발생
-    $(".header").addClass('main-style');
-    $(".footer").addClass('main-style');    
-
+    $header.addClass('main-style');
+    $footer.addClass('main-style'); 
 
 
     //메인 스크립트 - 서비스 영역 
-    $('.main-service > div').mouseenter(function(){
+    $service.mouseenter(function(){
       $(this).addClass('active');
       if ($(this).attr('class') == 'mychart active') {
         $(this).animate({height:315},300);
@@ -22,20 +26,20 @@ $(document).ready(function() {
       } 
     });
 
-    $('.main-service > div').mouseleave(function(){
+    $service.mouseleave(function(){
       if ( $(this).find('input').length >= 1) {
         if ($(this).find('input').val()=='') {    
-          $(this).animate({height:105},300);
-          $(this).removeClass('active');    
+          $(this).animate({height:105},300)
+          .removeClass('active');    
         }
       }else{
-        $(this).animate({height:105},300);
-        $(this).removeClass('active');
+        $(this).animate({height:105},300)
+        .removeClass('active');
       }
     });
 
 
-    //메인 상단 슬라이드 
+    //메인 상단 슬라이드 적용
     $('.now-lolling').bxSlider({
       auto: true,
       autoControls: true
@@ -46,21 +50,73 @@ $(document).ready(function() {
     });
 
 
+    //메인 네비 버튼 클릭시 //0 960 1920
+    $navBtn.on('click', function() {
+     var scrollPosition = $($(this).attr("data-target")).offset().top;
+     $navBtn.removeClass('active');
+     $(this).addClass('active');
+     $html.animate({scrollTop : scrollPosition}, 400);
+     console.log(scrollPosition);
+   });
+
+
+
+    var mainScroll = function(i, height){
+      //console.log("ss");
+      //$html.animate({scrollTop : height}, 400);
+      $navBtn.removeClass('active')
+      .eq(i).addClass('active');
+    }
+
+
+    //마우스 휠 업다운 이벤트 작업중
+    $('.main-wrap').bind('mousewheel', function(e){
+      if(e.originalEvent.wheelDelta /120 > 0) {
+        
+        console.log('scrolling up !');
+      }
+      else{
+        console.log('scrolling down !');
+      }
+    });
+
     
-  });
+    var height = $(document).scrollTop();
+
+    if ( height > 1 ) {
+       $header.removeClass('main-style')
+    .addClass('main-style2');
+    }else if ( height <= 500 ) {
+      mainScroll(0, 0);
+    }else if (500 < height <= 1400) {
+      mainScroll(1, 960);
+    }else if (1400 < height ) {
+      mainScroll(2, 1920);
+    }
 
 
+
+
+
+ });
 
 /* 스크롤 높이에 따라서 헤더 및 푸터 수정*/
 $(window).scroll(function() { 
+  var $header = $('.header');  
+  var $footer = $('.footer'); 
+  var height = $(document).scrollTop();
+  log(height);
 
 
-  if ($(this).scrollTop() >= 10) {
-    $header.removeClass('main-style');
-    $header.addClass('main-style2');
+  if ($(this).scrollTop() >= 1) {
+    $header.removeClass('main-style')
+    .addClass('main-style2');
   }else {
-    $header.addClass('main-style'); 
-    $header.removeClass('main-style2');
+    if ($('.opensection-wrap').css('display')=='none') {
+      $header.addClass('main-style')
+      .removeClass('main-style2'); 
+    }
+
   }
 
   if ($(this).scrollTop() >= 2010) {
@@ -73,11 +129,6 @@ $(window).scroll(function() {
 });
 
 
-
-
-
-
-
-
-
-
+function log(str){
+  $('#log').text(str);
+}
