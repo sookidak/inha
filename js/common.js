e2b783e6439d223cb32cacd70d7de564952d889c
ljhmw6 <chorika@naver.com>
2019-01-14 16:31:44 +0900

    윤리경영, 발전기금, 사회공헌 적용

--- js/common.js
+++ js/common.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	var $allBtn=$(".btn-allmenu"),
 	$closeBtn=$(".header .btn-close"),
-	$header = $('.header'),  
-	$footer = $('.footer'), 
-	$nav = $('.nav-btn'), 
+	$header = $('.header'),
+	$footer = $('.footer'),
+	$nav = $('.nav-btn'),
 	$navWrap=$(".nav-wrap"),
 	$mainWrap=$(".main-wrap"),
 	$opensectionWrap=$(".opensection-wrap"),
@@ -30,7 +30,7 @@ $(document).ready(function() {
 		}
 	}
 	//메뉴 열기
-	var openMenu = function(){	
+	var openMenu = function(){
 		$opensectionWrap.slideDown("fast")
 		.addClass('active');
 		$allBtn.hide();
@@ -40,62 +40,62 @@ $(document).ready(function() {
 		}
 	}
 	//메뉴 닫기
-	var closeMenu = function(){		
+	var closeMenu = function(){
 		if (allBtnNum==0) {
 			$opensectionWrap.stop().slideUp("fast")
 			.removeClass('active');
 			$allBtn.show();
 			$closeBtn.hide();
-			if (mainCheck() && $(this).scrollTop() == 0) {				
+			if (mainCheck() && $(this).scrollTop() == 0) {
 				$header.addClass('main-style')
-				.removeClass('main-style2')			
+				.removeClass('main-style2')
 			}
 		}
 	}
 
 	//전체버튼 클릭 이벤트
 	$allBtn.on('click', function(event) {
-		allBtnNum = 1;	
+		allBtnNum = 1;
 		openMenu();
 	});
 	$closeBtn.on('click', function(event) {
-		allBtnNum = 0;	
+		allBtnNum = 0;
 		closeMenu();
 	});
 	$mainWrap.on('click', function(event) {
-		allBtnNum = 0;	
+		allBtnNum = 0;
 		closeMenu();
 	});
 	$container.on('click', function(event) {
-		allBtnNum = 0;	
+		allBtnNum = 0;
 		closeMenu();
 	});
 	$footer.on('click', function(event) {
-		allBtnNum = 0;	
+		allBtnNum = 0;
 		closeMenu();
 	});
-	$resbtn.on('click', function(event) {		
+	$resbtn.on('click', function(event) {
 		if ($opensectionWrap.css("display")== "none") {
 			openMenu();
-			allBtnNum = 1;	
+			allBtnNum = 1;
 		} else {
-			allBtnNum = 0;	
-			closeMenu();			
+			allBtnNum = 0;
+			closeMenu();
 		}
 	});
 
-	//메뉴 롤오버 이벤트 
+	//메뉴 롤오버 이벤트
 	$gnb.on('mouseenter',function(){
-		openMenu();	
+		openMenu();
 	});
 	$navWrap.on('mouseleave',function(){
-		closeMenu();	
+		closeMenu();
 	});
 	$gnb.find('a').on('focus',function(){
 		openMenu();
 	});
 	$(".opensection-qbanner button").blur(function(){
-		closeMenu();	
+		closeMenu();
 	});
 
 	$(".lang").on('click',function(event) {
@@ -108,7 +108,7 @@ $(document).ready(function() {
 		$(this).addClass('active')
 		.siblings('gnb3Wrap').show();
 	});
-	$gnb2depth.on('mouseleave',function(){		
+	$gnb2depth.on('mouseleave',function(){
 		$(this).removeClass('active');
 	});
 
@@ -116,7 +116,7 @@ $(document).ready(function() {
 	$gnb3Wrap.on('mouseenter',function(){
 		$(this).siblings('a').addClass('active');
 	});
-	$gnb3Wrap.on('mouseleave',function(){		
+	$gnb3Wrap.on('mouseleave',function(){
 		$(this).siblings('a').removeClass('active');
 	});
 
@@ -140,9 +140,9 @@ $(document).ready(function() {
 	}
 
 
-	//탭 클릭 이벤트 
+	//탭 클릭 이벤트
 	$tablist1.find('a').on('click',function(e) {
-		e.preventDefault();		
+		e.preventDefault();
 		$tablist1.find('a').removeClass('active');
 		$(this).addClass('active');
 		$(".tabpanel").removeClass('active');
@@ -152,9 +152,9 @@ $(document).ready(function() {
 	});
 
 
-	//서브 탭 클릭 이벤트 
+	//서브 탭 클릭 이벤트
 	$tablist2.find('a').on('click',function(e) {
-		e.preventDefault();		
+		e.preventDefault();
 		$tablist2.find('a').removeClass('active');
 		$(this).addClass('active');
 		$(".tabpanel-sub").removeClass('active');
@@ -172,7 +172,7 @@ $(document).ready(function() {
 		scrollMove(196);
 	});
 	var scrollMove = function (n){
-		$('.tablist-type1').animate({scrollLeft : $('.tablist-type1').scrollLeft() + n}, 500);   
+		$('.tablist-type1').animate({scrollLeft : $('.tablist-type1').scrollLeft() + n}, 500);
 	}
 
 
@@ -187,7 +187,7 @@ $(document).ready(function() {
   	});
 
 
-	//진료과 목록  아이콘 노출 
+	//진료과 목록  아이콘 노출
 	$('.text-ko > a').on('focus',function(){
 		$('.over-icon').css( 'display', 'none' );
 		$(this).siblings('.over-icon').css( 'display', 'block' );
@@ -195,32 +195,71 @@ $(document).ready(function() {
 
 
 	//select disable적용
-	if ($("select").length){ 		
+	if ($("select").length){
 		$('select').each(function(i, e){
-			var sele = $(this).prop('disabled');		
-			if (sele==true) {		 			
+			var sele = $(this).prop('disabled');
+			if (sele==true) {
 				$(this).parent('.select').addClass('disabled');
 			}
 		});
 	}
-	//select 적용 
-	var selectTarget = $('.select select'); 
-	selectTarget.change(function(){ 
-		var select_name = $(this).children('option:selected').text(); 
-		$(this).siblings('label').text(select_name); 
+	//select 적용
+	var selectTarget = $('.select select');
+	selectTarget.change(function(){
+		var select_name = $(this).children('option:selected').text();
+		$(this).siblings('label').text(select_name);
 	});
 
 
 
+  //input 파일찾기
+	var fileTarget = $('.filebox .upload-hidden');
+
+	    fileTarget.on('change', function(){
+	        if(window.FileReader){
+	            // 파일명 추출
+	            var filename = $(this)[0].files[0].name;
+	        }
+
+	        else {
+	            // Old IE 파일명 추출
+	            var filename = $(this).val().split('/').pop().split('\\').pop();
+	        };
+
+	        $(this).siblings('.upload-name').val(filename);
+	    });
+
+	    //preview image
+	    var imgTarget = $('.preview-image .upload-hidden');
+
+	    imgTarget.on('change', function(){
+	        var parent = $(this).parent();
+	        parent.children('.upload-display').remove();
+
+	        if(window.FileReader){
+	            //image 파일만
+	            if (!$(this)[0].files[0].type.match(/image\//)) return;
+
+	            var reader = new FileReader();
+	            reader.onload = function(e){
+	                var src = e.target.result;
+	                parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img src="'+src+'" class="upload-thumb"></div></div>');
+	            }
+	            reader.readAsDataURL($(this)[0].files[0]);
+	        }
+
+	        else {
+	            $(this)[0].select();
+	            $(this)[0].blur();
+	            var imgSrc = document.selection.createRange().text;
+	            parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img class="upload-thumb"></div></div>');
+
+	            var img = $(this).siblings('.upload-display').find('img');
+	            img[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enable='true',sizingMethod='scale',src=\""+imgSrc+"\")";
+	        }
+	    });
 
 
 
 
 });
-
-
-
-
-
-
-
